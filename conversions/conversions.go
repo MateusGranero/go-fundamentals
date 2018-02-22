dfbfbd05745b747f23c3242a73e30a8b79acb478
Mateus Granero <mateusgranero@gmail.com>
2018-02-22 11:36:54 -0300

    Type conversions

---
+++ conversions/conversions.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	x := 2.4
+	y := 2
+	fmt.Println(x / float64(y))
+
+	nota := 6.9
+	notafinal := int(nota)
+	fmt.Println(notafinal)
+
+	//cuidado...
+
+	fmt.Println("Teste " + string(123)) // codigo tabela ASC
+
+	//int para string
+	fmt.Println("Teste " + strconv.Itoa(123))
+
+	//string para int
+	num, _ := strconv.Atoi("123")
+	fmt.Println(num - 122)
+
+	//bool
+	b, _ := strconv.ParseBool("true")
+	if b {
+		fmt.Println("Verdadeiro")
+	}
+}
